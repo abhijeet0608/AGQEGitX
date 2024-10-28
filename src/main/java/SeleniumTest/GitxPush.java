718f656a7671c0b297ff5df4f20305d76480c472
abhijeet0608 <abhijeetgavali7297@gmail.com>
2024-10-28 15:25:36 +0530

    Local commit

---
+++ src/main/java/SeleniumTest/GitxPush.java
@@ -0,0 +1,10 @@
+package SeleniumTest;
+
+public class GitxPush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Abhijeet Local Code");
+	}
+
+}
